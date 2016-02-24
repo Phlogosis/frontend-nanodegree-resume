7c7940e202fb3eb94cd1f431cf4643be815cdfd8
Markars <karamar.joseph@gmail.com>
2016-02-24 13:30:48 -0500

    Added JSON data objects and changed empty sections to become purple

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,64 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+	"name" : "Karamar Joseph",
+	"role" : "Front End Web Developer",
+	"contact" :
+	{
+		"email" : "karamar.joseph@gmail.com"
+	},
+	"bioPic" : "images/goateeedit.jpg",
+	"welcomeMsg" : "a new solution for a new day",
+	"skills" : ["programming", "web development", "customer service"]
+}
+
+var work = {
+	"jobs" :
+	[
+		{
+		"employer" : "Boston Bread LLC",
+		"title" : "Associate Trainer",
+		"location" : "Watertown, MA",
+		"dates" : ["June 2013", "Present"],
+		"description" : "Customer service, food prep, recieving and filling orders"
+		}
+	]
+}
+
+var education = {
+	"schools" :
+	[
+		{
+		"name" : "Massachussets Bay Community College",
+		"location" : "Wellesley, MA",
+		"degree" : "TBD",
+		"majors" : ["Information Sciences and Technology"],
+		"dates" : ["Sept 2011", "May 2013"],
+		"url" : "http://www.massbay.edu"
+		}
+	]
+	"onlineCourses" :
+	[
+		{
+		"title" : "Front-End Web Development",
+		"school" : "Udacity",
+		"dates" : ["Jan 2016", "TBD"]
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+	{
+		"title" : "Project title",
+		"dates" : ["Beginning", "End"],
+		"description" : "Something came in the mail today.",
+		"images" : ["images/these.jpg"]
+	}
+	]
+
+}
+
+var data = "%data%";
+
