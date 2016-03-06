3ecbf30d9a38786d934318c2463650156e355fa6
Markars <karamar.joseph@gmail.com>
2016-03-06 02:36:34 -0500

    linted the bio object and removed some extra code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,134 +1,130 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var data = "%data%";
+
 var bio = {
-	"name" : "Karamar Joseph",
-	"role" : "Front End Web Developer",
-	"contact" :
-	{
-		"email" : "karamar.joseph@gmail.com"
-	},
-	"bioPic" : "images/goateeedit.jpg",
-	"welcomeMsg" : "a new solution for a new day",
-	"skills" : ["programming", "web development", "customer service"]
+    "name" : "Karamar Joseph",
+    "role" : "Front End Web Developer",
+    "contacts" : {
+        "mobile" : "XXX-YYY-ZZZZ",
+        "email" : "karamar.joseph@gmail.com",
+        "github" : "http://github.com/phlogosis",
+        "twitter" : "no",
+        "location" : "Waltham, MA"
+    },
+    "welcomeMessage" : "a new solution for a new day",
+    "skills" : [
+        "programming", "web development", "customer service"
+    ],
+    "biopic" : "images/goateeedit.jpg",
+    "display" : function() {
+        var bioName = HTMLheaderName.replace(data, bio.name);
+        var bioRole = HTMLheaderRole.replace(data, bio.role);
+        $("#header").prepend(bioName + bioRole); // It -is- okay to concatenate.
+    }
 }
 
 var work = {
-	"jobs" :
-	[
-		{
-		"employer" : "Panera Bread",
-		"title" : "Associate Trainer",
-		"location" : "Watertown, MA",
-		"dates" : ["June 2013", "Present"],
-		"description" : "Customer service, food prep, recieving and filling orders"
-		},
-	
-		{
-		"employer" : "Waltham Public Library",
-		"title" : "Page",
-		"location" : "Waltham, MA",
-		"dates" : ["Sept 2010", "Apr 2011"],
-		"description" : "Recieving imports, reshelving entire rooms"
-		}
-	]
+    "jobs" :
+    [
+        {
+        "employer" : "Panera Bread",
+        "title" : "Associate Trainer",
+        "location" : "Watertown, MA",
+        "dates" : ["June 2013", "Present"],
+        "description" : "Customer service, food prep, recieving and filling orders"
+        },
+
+        {
+        "employer" : "Waltham Public Library",
+        "title" : "Page",
+        "location" : "Waltham, MA",
+        "dates" : ["Sept 2010", "Apr 2011"],
+        "description" : "Recieving imports, reshelving entire rooms"
+        }
+    ]
 }
 
 var education = {
-	"schools" :
-	[
-		{
-		"name" : "Massachussets Bay Community College",
-		"location" : "Wellesley, MA",
-		"degree" : "TBD",
-		"majors" : ["Information Sciences and Technology"],
-		"dates" : ["Sept 2011", "May 2013"],
-		"url" : "http://www.massbay.edu"
-		}
-	],
-	"onlineCourses" :
-	[
-		{
-		"title" : "Front-End Web Development",
-		"school" : "Udacity",
-		"dates" : ["Jan 2016", "TBD"]
-		}
-	]
+    "schools" :
+    [
+        {
+        "name" : "Massachussets Bay Community College",
+        "location" : "Wellesley, MA",
+        "degree" : "TBD",
+        "majors" : ["Information Sciences and Technology"],
+        "dates" : ["Sept 2011", "May 2013"],
+        "url" : "http://www.massbay.edu"
+        }
+    ],
+    "onlineCourses" :
+    [
+        {
+        "title" : "Front-End Web Development",
+        "school" : "Udacity",
+        "dates" : ["Jan 2016", "TBD"]
+        }
+    ]
 }
 
 var projects = {
-	"projects" : [
-	{
-		"title" : "Project title",
-		"dates" : ["Beginning", "End"],
-		"description" : "Something came in the mail today.",
-		"images" : ["images/these.jpg"]
-	},
-	{
-		"title" : "Other Project",
-		"dates" : ["Starting", "Finishing"],
-		"description" : "Would you like a pizza?",
-		"images" : ["images/pizzathis.jpg"]
-	}
-	]
+    "projects" : [
+    {
+        "title" : "Project title",
+        "dates" : ["Beginning", "End"],
+        "description" : "Something came in the mail today.",
+        "images" : ["images/these.jpg"]
+    },
+    {
+        "title" : "Other Project",
+        "dates" : ["Starting", "Finishing"],
+        "description" : "Would you like a pizza?",
+        "images" : ["images/pizzathis.jpg"]
+    }
+    ]
 
 }
 
 
-var data = "%data%";
 
 if (bio.hasOwnProperty("skills")) {
-	if (bio.skills.length > 0) {
-		$("#header").append(HTMLskillsStart);
-		for (skill in bio.skills) {
-			$("#skills").append(HTMLskills.replace(data, 
-						bio.skills[skill]));
-		}
-	}
+    if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+        for (skill in bio.skills) {
+            $("#skills").append(HTMLskills.replace(data,
+                        bio.skills[skill]));
+        }
+    }
 }
 
 for (job in work.jobs) {
     $("#workExperience").append(HTMLworkStart);
-    var jobEmployerAndTitle = 
+    var jobEmployerAndTitle =
         HTMLworkEmployer.replace(data, work.jobs[job].employer)
             + HTMLworkTitle.replace(data, work.jobs[job].title);
     var jobDates =
         HTMLworkDates.replace(data,
-			work.jobs[job].dates[0]
-			+ " - "
-			+ work.jobs[job].dates[1]);
-    var jobLocation = 
+            work.jobs[job].dates[0]
+            + " - "
+            + work.jobs[job].dates[1]);
+    var jobLocation =
         HTMLworkLocation.replace(data, work.jobs[job].location);
-    var jobDescription = 
-	HTMLworkDescription.replace(data, work.jobs[job].description);
-    $(".work-entry:last").append(jobEmployerAndTitle + jobDates 
-		    + jobLocation + jobDescription);
+    var jobDescription =
+    HTMLworkDescription.replace(data, work.jobs[job].description);
+    $(".work-entry:last").append(jobEmployerAndTitle + jobDates
+            + jobLocation + jobDescription);
 }
 
 $("#main").append(internationalizeButton);
 
-function inName(firstandlast) {
-	var newname = firstandlast.split(" ");
-        var newnewname = [];
-	newnewname[0] = newname[0].slice(0,1).toUpperCase() 
-	+ newname[0].slice(1).toLowerCase();
-	newnewname[1] = newname[1].toUpperCase();
-
-	return newnewname[0] + " " + newnewname[1];
-}
-
-
 projects.display = function() {
-    //$("#projects").append(HTMLprojectStart);
-    //$(".project-entry:last").append(HTMLprojectTitle.replace(data,projects.projects[0].title));
     for (proj in projects.projects) {
-	    $("#projects").append(HTMLprojectStart);
-	    var projTitle = HTMLprojectTitle.replace(data,projects.projects[proj].title);
-	    var projDates = HTMLprojectDates.replace(data, projects.projects[proj].dates[0] + " - " + projects.projects[proj].dates[1]);
-	    var projDescription = HTMLprojectDescription.replace(data, projects.projects[proj].description);
-	    var projImage = HTMLprojectImage.replace(data, projects.projects[proj].image);
-	    $(".project-entry:last").append(projTitle + projDates + projDescription + projImage);
+        $("#projects").append(HTMLprojectStart);
+        var projTitle = HTMLprojectTitle.replace(data,projects.projects[proj].title);
+        var projDates = HTMLprojectDates.replace(data, projects.projects[proj].dates[0] + " - " + projects.projects[proj].dates[1]);
+        var projDescription = HTMLprojectDescription.replace(data, projects.projects[proj].description);
+        var projImage = HTMLprojectImage.replace(data, projects.projects[proj].image);
+        $(".project-entry:last").append(projTitle + projDates + projDescription + projImage);
     }
 };
 
+bio.display();
 projects.display();
